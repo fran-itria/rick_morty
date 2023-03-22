ec34d0bcb3fecac42d4dcc17592ada6ec8ff386d
Franco Itria <francoitria01@gmail.com>
2023-03-22 10:04:31 -0300

    cahnges in my server

--- Back/src/server.js
+++ Back/src/server.js
@@ -4,9 +4,9 @@ const characters = require('./utils/data')
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     const { url } = req
-    const id = url.trim().split('/').pop()
     if (url.includes('rickandmorty/character')) {
-        res.writeHead(200, { 'Content-Type': 'aplication/json' })
-        res.end(JSON.stringify(characters.find(char => char.id == id)))
-    } else {return res.end(404, 'Id no encontrado')}
+        const id = url.trim().split('/').pop()
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        return res.end(JSON.stringify(characters.find(char => char.id == id)))
+    } else { return res.end(404, 'Id no encontrado') }
 }).listen(3001, 'localhost')
