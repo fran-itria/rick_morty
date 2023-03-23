e5f6694c18f1f630a13ad307063afab5e578cb39
Franco Itria <francoitria01@gmail.com>
2023-03-23 18:40:55 -0300

    handler errors on my server

--- Back/src/controllers/getCharacterById.js
+++ Back/src/controllers/getCharacterById.js
@@ -1,19 +1,19 @@
 const axios = require('axios')
 
+const URL_BASE = 'https://rickandmortyapi.com/api'
+const KEY = '179180d9d086.4e91a167f3c86bcbbb24';
+
 const getCharByID = (res, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`)
+    axios(`${URL_BASE}/character/${id}?key=${KEY}`)
         .then(response => {
             const { id, name, species, gender, image } = response.data
-            const information = {
-                id,
-                name,
-                species,
-                gender,
-                image
-            }
-            res.end(JSON.stringify(information))
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end(JSON.stringify({ id, name, species, gender, image }))
         })
-    // .catch(res.end(500, { 'Content-Type': 'text/plain' }), { message: error.message })
+        .catch(reason => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            res.end(reason.message)
+        })
 }
 
 module.exports = getCharByID
--- Back/src/controllers/getCharactersDetail.js
+++ Back/src/controllers/getCharactersDetail.js
@@ -1,21 +1,18 @@
 const axios = require('axios')
 
+const URL_BASE = 'https://rickandmortyapi.com/api'
+const KEY = '179180d9d086.4e91a167f3c86bcbbb24';
+
 const getCharDetail = (res, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`)
+    axios(`${URL_BASE}/character/${id}?key=${KEY}`)
         .then(response => {
-            const { name, species, gender, status, origin, image, episode} = response.data
-            const information = {
-                name,
-                species,
-                gender,
-                status,
-                origin,
-                image,
-                episode
-            }
-            res.end(JSON.stringify(information))
+            const { name, species, gender, status, origin, image, episode } = response.data
+            res.end(JSON.stringify({ name, species, gender, status, origin, image, episode }))
         })
-    // .catch(res.end(500, { 'Content-Type': 'text/plain' }), { message: error.message })
+        .catch(reason => {
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            res.edn(reason.message)
+        })
 }
 
 module.exports = getCharDetail
