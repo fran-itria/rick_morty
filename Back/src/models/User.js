977a22b0147bcb387c9bba0da9bb0e2e9365ecce
Franco Itria <francoitria01@gmail.com>
2023-04-07 04:15:52 -0300

    Created my data base

---
+++ Back/src/models/User.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+   sequelize.define('User', {
+      id: {
+         type: DataTypes.INTEGER,
+         autoIncrement: true,
+         primaryKey: true
+      },
+      email: {
+         type: DataTypes.STRING,
+         unique: true,
+         allowNull: false
+      },
+      password: {
+         type: DataTypes.STRING,
+         unique: true,
+         allowNull: false
+      }
+   }, { timestamps: false });
+};
--- Back/src/server.js
+++ Back/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes/index')
+const { conn } = require('./DB_connection')
 const server = express()
 const PORT = 3001
 
@@ -8,4 +9,9 @@ server.use(express.json())
 server.use(cors())
 server.use('/', router)
 
-server.listen(PORT, () => console.log(`Correindo en el puerto ${PORT}`))
+conn
+    .sync({ alter: true })
+    .then(() => {
+        server.listen(PORT, () => console.log(`Correindo en el puerto ${PORT}`))
+    })
+    .catch(error => console.log(error))
