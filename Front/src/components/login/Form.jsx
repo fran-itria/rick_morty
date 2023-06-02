4e86a907242b32e6942fbcde393e7c4ecd1be610
Franco Itria <francoitria01@gmail.com>
2023-06-02 09:31:29 -0300

    Inicio y registro de usuario

--- Front/src/components/login/Form.jsx
+++ Front/src/components/login/Form.jsx
@@ -1,52 +1,152 @@
-import React, { useEffect } from 'react'
-import style from './Form.module.css'
-import { validate } from '../../functions/functions.js'
+import React, { useEffect, useState } from "react";
+import style from "./Form.module.css";
+import { validate } from "../../functions/functions.js";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { handlerSession } from "../redux/actions";
 
-export default function Form({ login, setaccess, navigate }) {
-    const [inputs, setInputs] = React.useState({
-        username: '',
-        password: ''
-    })
-    const [errors, setErrors] = React.useState({
-        username: '',
-        password: ''
-    })
+export default function Form({setAccess, navigate }) {
+  const [select, setSelect] = useState("login");
+  const [inputs, setInputs] = React.useState({
+    username: "",
+    password: "",
+  });
+  const [errors, setErrors] = React.useState({
+    username: "",
+    password: "",
+  });
+  const dispatch = useDispatch()
 
-    function handleChange(event) {
-        setInputs({ ...inputs, [event.target.name]: event.target.value })
-        setErrors(validate({ ...inputs, [event.target.name]: event.target.value }))
+  function handleChange(event) {
+    setInputs({ ...inputs, [event.target.name]: event.target.value });
+    setErrors(validate({ ...inputs, [event.target.name]: event.target.value }));
+  }
+  async function handleAccess(event, inputs) {
+    event.preventDefault();
+    const { username, password } = inputs;
+    try {
+      const user = await axios(`login?email=${username}&password=${password}`);
+      if (user) {
+        setAccess(true)
+        navigate('/about')
+        dispatch(handlerSession(inputs.username))
+      };
+    } catch (error) {
+      const alert = error.response.data.error;
+      window.alert(alert);
     }
-    function handleSubmit() {
-        login(inputs, setaccess, navigate)
+  }
+  async function handleRegister(event) {
+    event.preventDefault();
+    try {
+      const newUser = await axios.post(`login`, inputs);
+      if(newUser) {
+        console.log(newUser)
+        window.alert('Registro exitoso')
+      }
+    } catch (error) {
+      const alert = error.response.data.error;
+      window.alert(alert);
     }
-    return (
-        <div className={style.conteiner}>
-            <form className={style.form} onSubmit={() => handleSubmit()}>
-                <div className={style.inputs}>
-                    <div>
-                        {errors.username && <p className={style.warning}>¡{errors.username}!</p>}
-                        <label className={style.label}>Username:</label>
-                        <input
-                            className={errors.username ? style.danger : style.input}
-                            value={inputs.username}
-                            name='username'
-                            onChange={handleChange}>
-                        </input>
-                    </div>
-                    <div>
-                        {errors.password && <p className={style.warning}>¡{errors.password}!</p>}
-                        <label className={style.label}>Password:</label>
-                        <input
-                            className={errors.password ? style.danger : style.input}
-                            value={inputs.password}
-                            type='password'
-                            name='password'
-                            onChange={(event) => { handleChange(event) }}>
-                        </input>
-                    </div>
-                </div>
-                <button className={(errors.username || errors.password) ? style.disabled : style.button} type='submit'>LOGIN</button>
-            </form>
-        </div>
-    )
+  }
+  function handleForm(event) {
+    setSelect(event.target.name);
+  }
+  return (
+    <div className={style.conteiner}>
+      <div className={style.select}>
+        <button name="login" onClick={handleForm}>
+          Login
+        </button>
+        <button name="register" onClick={handleForm}>
+          Register
+        </button>
+      </div>
+      {select == "login" ? ( // FORMULARIO DE LOGIN
+        <form
+          className={style.form}
+          onSubmit={(event) => handleAccess(event, inputs)}
+        >
+          <div className={style.inputs}>
+            <div>
+              {errors.username && (
+                <p className={style.warning}>¡{errors.username}!</p>
+              )}
+              <label className={style.label}>Username:</label>
+              <input
+                className={errors.username ? style.danger : style.input}
+                value={inputs.username}
+                name="username"
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div>
+              {errors.password && (
+                <p className={style.warning}>¡{errors.password}!</p>
+              )}
+              <label className={style.label}>Password:</label>
+              <input
+                className={errors.password ? style.danger : style.input}
+                value={inputs.password}
+                type="password"
+                name="password"
+                onChange={(event) => {
+                  handleChange(event);
+                }}
+              ></input>
+            </div>
+          </div>
+          <button
+            className={
+              errors.username || errors.password ? style.disabled : style.button
+            }
+            type="submit"
+          >
+            LOGIN
+          </button>
+        </form>
+      ) : (
+        // FROMULARIO DE REGISTRO
+        <form className={style.form} onSubmit={(event) => handleRegister(event)}>
+          <div className={style.inputs}>
+            <div>
+              {errors.username && (
+                <p className={style.warning}>¡{errors.username}!</p>
+              )}
+              <label className={style.label}>Username:</label>
+              <input
+                className={errors.username ? style.danger : style.input}
+                value={inputs.username}
+                name="username"
+                onChange={handleChange}
+              ></input>
+            </div>
+            <div>
+              {errors.password && (
+                <p className={style.warning}>¡{errors.password}!</p>
+              )}
+              <label className={style.label}>Password:</label>
+              <input
+                className={errors.password ? style.danger : style.input}
+                value={inputs.password}
+                type="password"
+                name="password"
+                onChange={(event) => {
+                  handleChange(event);
+                }}
+              ></input>
+            </div>
+          </div>
+          <button
+            className={
+              errors.username || errors.password ? style.disabled : style.button
+            }
+            type="submit"
+          >
+            REGISTER
+          </button>
+        </form>
+      )}
+    </div>
+  );
 }
--- Front/src/functions/functions.js
+++ Front/src/functions/functions.js
@@ -1,5 +1,3 @@
-import { DETAIL_CHARACTER } from "../components/redux/actions";
-
 ////////////////////////////////
 
 // FUNCION SEARCH APP
@@ -18,22 +16,13 @@ export function onSearch(characterId, characters, setCharacters) {
 
 //  <---- FUNCTIONS FORM ---->
 
-// LOGIN FORM
-const username = 'FrancoItria01@gmail.com'
-const password = 'pepito01'
-export function login(userData, setAccess, navigate) {
-    if (userData.password == password && userData.username == username) {
-        setAccess(true)
-        navigate('/about')
-    } else window.alert('Correo o contraseña incorrectos')
-}
 // VALIDATE FORM 
 export function validate(inputs) {
     let errors = {}
     const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{3}$/i
     if (!regexEmail.test(inputs.username) || inputs.username.length > 35) errors.username = 'Debe ser un correo electronico'
     if (regexEmail.test(inputs.username) && inputs.username.length > 35) errors.username = 'Debe ser menor a 35 carateres'
-    if (!inputs.password.match(/\d/)) errors.password = 'Debe contener al menos un numero'
-    if (inputs.password.match(/\d/) && (inputs.password.length < 6 || inputs.password.length > 8)) errors.password = 'Debe contener entre 6 y 8 caracteres'
+    if (inputs.password.length < 6) errors.password = 'Debe contener mínimo 6 caracteres'
+    if (inputs.password.length >= 6 && !inputs.password.match(/\d/)) errors.password = 'Debe contener al menos un numero'
     return errors
 }
