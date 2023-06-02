dd8c459bae414ec5b2862903b8f276d3378255bf
Franco Itria <francoitria01@gmail.com>
2023-06-02 09:32:53 -0300

    Boton de cerrar sesión agregado

--- Front/src/components/nav/Nav.jsx
+++ Front/src/components/nav/Nav.jsx
@@ -3,11 +3,21 @@ import SearchBar from "../search/SearchBar";
 import style from './Nav.module.css'
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { sessionClose } from "../redux/actions";
 
-export default function Nav({ onSearch, characters, setCharacters }) {
+export default function Nav({ onSearch, characters, setCharacters, setAccess, navigate }) {
     const background = useSelector(state => state.component)
+    const dispatch = useDispatch()
+    const handleSession = () => {
+        dispatch(sessionClose())
+        setAccess(false)
+        navigate('/')
+    } 
     return (
         <div className={background == 'About' ? style.About : style.Cards}>
+            <div>
+                <button onClick={() => handleSession()}>Cerrar sesión</button>
+            </div>
             <div className={style.about}>
                 <NavLink to='/about' className={({ isActive }) => isActive ? style.active : style.disable}>About</NavLink>
             </div>
